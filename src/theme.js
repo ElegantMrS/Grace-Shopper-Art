6ba2fba7d2aed947db2163678532feef459954ee
ElegantMrS <josephjumber@gmail.com>
2021-07-26 16:37:10 -0400

    npm installed ready to commit

---
+++ src/theme.js
@@ -0,0 +1,31 @@
+// Joe
+
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#b6ffff',
+      main: '#df7332',
+      dark: '#1E7827',
+      contrastText: '#323232',
+    },
+    secondary: {
+      light: '#C71212',
+      main: '#155de9',
+      dark: '#0035b6',
+      contrastText: '#C7C7C7',
+    },
+    overrides: {
+        MuiCssBaseline: {
+          "@global": {
+            body: {
+              background: '#323232'
+            }
+          }
+        }
+      }
+  },
+});
+
+export default theme;
